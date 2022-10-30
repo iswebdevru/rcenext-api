cfd2e3b3a1169267eb24052ae4b1ec3a051d54bd
wardxela <axelwardweb@gmail.com>
2022-10-30 15:37:18 +0300

    refactor: remove useless files, change README

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GroupModule } from './group/group.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
   imports: [PrismaModule, GroupModule],
 })
 export class AppModule {}
